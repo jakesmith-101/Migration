5384b6abc231539040c182c3d4b3bdcd11772e04
ZiarayZ <jakesmith@binaryrage.com>
2024-08-11 17:19:32 +0100

    main

--- proxy.go
+++ proxy.go
@@ -16,3 +16,16 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
 }
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	<^>}
+}
