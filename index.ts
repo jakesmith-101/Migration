ed51f0aba1e28c36af53fdcdfd969e20e861b12b
ZiarayZ <jakesmith@binaryrage.com>
2024-08-13 16:15:07 +0100

    params setup

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import express, { RequestHandler } from 'express';
+import createHttpError from 'http-errors';
 import QueryString from 'qs';
 
-type tHandler = RequestHandler<{}, any, any, QueryString.ParsedQs, Record<string, any>>;
+type tHandler<T> = RequestHandler<T, any, any, QueryString.ParsedQs, Record<string, any>>;
 
 const app = express()
 const port = 8888
 
 // storing orders in memory isntead of DB as an example
-const orders = [];
+const orders: { id: string, items: string[] }[] = [];
 
-const order: { [key: string]: tHandler } = {
+const order: { [key: string]: tHandler<{ orderID?: string }> } = {
     // get order list
     index: (req, res) => {
 
@@ -18,13 +19,15 @@ const order: { [key: string]: tHandler } = {
 
     // view order
     view: (req, res) => {
-
-        res.json();
+        const fOrder = orders.find(o => o.id === req.params.orderID);
+        if (fOrder !== undefined)
+            res.json(fOrder);
+        else
+            res.status(404).json(null);
     },
 
     // create or update order
     save: (req, res) => {
-        req
 
         res.json();
     },
@@ -36,14 +39,25 @@ const order: { [key: string]: tHandler } = {
     }
 }
 
+app.param('orderID', function (req, res, next, id) {
+    req.params['orderID'] = id;
+    if (typeof req.params['orderID'] !== 'string') {
+        next(createHttpError(400, 'failed to parseString ' + id));
+    } else {
+        next();
+    }
+});
+
 app.map({
     '/api/order': {
-        get: order.index,
+        get: order.index, // order list
+        post: order.save, // create order
+        put: order.save, // create order
         '/:orderID': {
-            get: order.view,
-            post: order.save,
-            put: order.save,
-            delete: order.remove
+            get: order.view, // view order
+            post: order.save, // edit order
+            put: order.save, // edit order
+            delete: order.remove // delete order
         }
     }
 });
