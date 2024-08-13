03ebcf55884c5d8855a6833640bb45f507bd9564
ZiarayZ <jakesmith@binaryrage.com>
2024-08-13 15:01:39 +0100

    format

--- index.ts
+++ index.ts
@@ -3,22 +3,29 @@ import express from 'express';
 const app = express()
 const port = 8888
 
+// get order list
 app.get('/order/', (req, res) => {
     res.send("Welcome to my orders.")
 })
 
-app.get('/order/create', (req, res) => {
+// create or update order
+function saveOrder(req, res) {
+
+}
+app.post('/order/:orderID', saveOrder)
+app.put('/order/:orderID', saveOrder)
+
+// view order
+app.get('/order/:orderID', (req, res) => {
 
 })
 
-app.get('/order/delete', (req, res) => {
+// delete order
+app.delete('/order/:orderID', (req, res) => {
 
 })
 
-app.get('/order/purchase', (req, res) => {
-
-})
-
+// listen for requests
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
