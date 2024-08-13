daa4808856bdb332d6b0d5d0544a9e36418b4103
ZiarayZ <jakesmith@binaryrage.com>
2024-08-13 15:04:34 +0100

    setup json responses

--- index.ts
+++ index.ts
@@ -3,14 +3,19 @@ import express from 'express';
 const app = express()
 const port = 8888
 
+// storing orders in memory isntead of DB as an example
+const orders = [];
+
 // get order list
 app.get('/api/order/', (req, res) => {
-    res.send("Welcome to my orders.")
+
+    res.json();
 })
 
 // create or update order
 function saveOrder(req, res) {
 
+    res.json();
 }
 app.post('/api/order/:orderID', saveOrder)
 app.put('/api/order/:orderID', saveOrder)
@@ -18,11 +23,13 @@ app.put('/api/order/:orderID', saveOrder)
 // view order
 app.get('/api/order/:orderID', (req, res) => {
 
+    res.json();
 })
 
 // delete order
 app.delete('/api/order/:orderID', (req, res) => {
 
+    res.json();
 })
 
 // listen for requests
